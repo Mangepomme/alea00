16354c20f880d4d3a0a8c503b8945088dddeb7cd
sebastien .g <sebastien .g>
2016-02-24 00:07:21 +0100

    J'ai rechargé le projet (enfin je crois). Il faut appuyer sur Ctrl pour faire apparaitre des boulles...

---
+++ Alea00/Assets/Tire.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Tire initialisé");
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        Debug.Log("Tire Effectué");
+        if (Input.GetButtonDown("Fire1"))
+        {
+            float xAxe = this.transform.position.x;
+            float yAxe = this.transform.position.y;
+            float zAxe = this.transform.position.z;
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = new Vector3(xAxe, yAxe, zAxe);
+        }
+    }
+}
