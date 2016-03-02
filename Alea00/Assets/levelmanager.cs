5c9bb140b7161ce5f85e14bcb0fb9babc580f1be
leo davoine <leo davoine>
2016-03-03 00:04:56 +0100

    le script que j'ai oublié d'ajouter tout à l'heure

---
+++ Alea00/Assets/levelmanager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelmanager : MonoBehaviour {
+
+    public Transform main_menu, options_menu;
+
+    public void Loadplaysolo()
+    {
+        Application.LoadLevel("SoloGame");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Optionsmenu(bool clicked)
+    {
+        if (clicked)
+        {
+            options_menu.gameObject.SetActive(true);
+            main_menu.gameObject.SetActive(false);
+        }
+        else
+        {
+            options_menu.gameObject.SetActive(false);
+            main_menu.gameObject.SetActive(true);
+        }
+    }
+}
