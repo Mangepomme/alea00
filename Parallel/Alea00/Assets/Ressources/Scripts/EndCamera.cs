ea52fa89f04508d7f5e75e43d0af8fec1eb34778
Gabriel Depotte <Gabriel Depotte>
2016-06-21 20:38:09 +0200

    Première version avec tous les niveaux !

--- Parallel/Alea00/Assets/Ressources/Scripts/EndCamera.cs
+++ Parallel/Alea00/Assets/Ressources/Scripts/EndCamera.cs
@@ -10,21 +10,34 @@ public class EndCamera : MonoBehaviour {
         if(s == "Win")
         {
             transform.position = new Vector3(500, 0, -100);
+
+            string url = "";
+            url += "http://alea00.comlu.com/ADD-SCORE.php?GAMEMODE=";
+            string lvl = PlayerPrefs.GetInt("Level").ToString();
+            url += lvl;
+            url += "&PSEUDO=";
+            string pseudo = PlayerPrefs.GetString("Pseudo");
+            url += pseudo;
+            url += "&PLANTYPE=";
+            string plane = "Fighter";
+            /*int p = PlayerPrefs.GetInt("Plane");
+            if(p == )*/
+            url += plane;
+            url += "&SCORE=";
+            url += PlayerPrefs.GetInt("Score").ToString();
+
+            Application.OpenURL(url);
         }
         else
         {
             transform.position = new Vector3(0, 0, -100);
         }
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        /*string s = PlayerPrefs.GetString("End");
-        Debug.Log(s);
-        if (s == "Win")
-        {
-            transform.position = new Vector3(500, 0, -100);
-        }*/
+
     }
 }
---
+++ Parallel/Alea00/Assets/Ressources/Scripts/PseudoInputScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PseudoInputScript : MonoBehaviour {
+
+    public InputField pseudoInput;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        string currentpseudo = pseudoInput.text;
+        if(currentpseudo != "")
+        {
+            PlayerPrefs.SetString("Pseudo", currentpseudo);
+        }
+        else
+        {
+            PlayerPrefs.SetString("Pseudo", "Pseudo");
+        }
+	}
+}
